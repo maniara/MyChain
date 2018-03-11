f4fdbfaf38f6d4a2a9c6463707258792b109af1d
KoDeokyoon <dykoh@finotek.co.kr>
2018-03-11 22:16:21 +0900

    comment added

--- mychain.py
+++ mychain.py
@@ -8,5 +8,9 @@ def signal_handler(_signal, frame):
 #프라이빗 블록체인의 경우 ip 리스트가 필요함
 ip_list = ["192.168.0.37","192.168.0.20"]
 app_controller.start_app(ip_list, isPrivate=True)
+
+#앱 종료를 위한 시그널 생성 (ctrl+c)
 signal.signal(signal.SIGINT, signal_handler)
+
+#메뉴 호출
 user_interface.main_menu()
